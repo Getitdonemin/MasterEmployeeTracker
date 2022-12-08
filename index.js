e2c4bf9d9f9b25ad6af38b788b550a559d819966
Your Name <getitdonemin@gmail.com>
2022-12-08 06:19:21 -0600

    Made all functions within index.js completing the prompt

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const manageDB = require("./manageDB");
 
 function masterEmployeeTracker() {
 
-    // This file prompts the user to Attach/delete/update employees/departments
-    // from the sql database.
+    // This file prompts to Attach/delete/update employees/departments from sql database
 
     inquirer
         .prompt([
@@ -27,7 +26,71 @@ function masterEmployeeTracker() {
                     "Exit"]
             },
         ])
+        .then(function (response) {
 
+            switch (response.databaseMenu) {
+                default:
+                    text = "Uh oh!";
+                    break;
+                case "Oversee department list":
+                    manageDB.overseeDept(function () {
+                        //reasking the questions for all other choices
+                        masterEmployeeTracker();
+                    });
+                    break;
+                case "Oversee roles":
+                    manageDB.overseeRoles(function () {
+                        masterEmployeeTracker();
+                    });
+                    break;
+                case "Oversee employee list":
+                    manageDB.overseeEmployees(function () {
+                        masterEmployeeTracker();
+                    });
+                    break;
+                case "Attach a department":
+                    manageDB.newDept(function () {
+                        masterEmployeeTracker();
+                    });
+                    break;
+                case "Attach a role":
+                    manageDB.newRole(function () {
+                        masterEmployeeTracker();
+                    });
+                    break;
+                case "Attach an employee":
+                    manageDB.newEmployee(function () {
+                        masterEmployeeTracker();
+                    });
+                    break;
+                case "Update employee role":
+                    manageDB.updateEmployeeRole(function () {
+                        masterEmployeeTracker();
+                    });
+                    break;
+                case "Update employee's manager":
+                    manageDB.updateEmployeeManager(function () {
+                        masterEmployeeTracker();
+                    });
+                    break;
+                case "Delete role from database":
+                    manageDB.DeleteRole(function () {
+                        masterEmployeeTracker();
+                    });
+                    break;
+                case "Delete employee from database":
+                    manageDB.DeleteEmployee(function () {
+                        masterEmployeeTracker();
+                    });
+                    break;
+                case "Exit":
+                    console.log("Quiting database");
+                    manageDB.afterConnection(function () {
+                        process.exit();
+                    });
+                    break;
+            }
+        });
 };
 
 masterEmployeeTracker();
